4b1e708631554bfe3c8bb8f509cca644c71e18d3
IlanWS <ilan.smeerdijk@gmail.com>
2026-04-13 14:59:59 +0200

    zutphenAndDeventerData

--- Import_annotation_from_json.py
+++ Import_annotation_from_json.py
@@ -4,8 +4,10 @@ import numpy as np
 from PIL import Image, ImageDraw
 from config import *
 
-json_path = "".join([os.getcwd(),"/Data/JSON_files/zutphen_1000_annotation.json"])
-output_dir = "".join([os.getcwd(),"/Data/Labels/zutphen_1000/"])
+name = "zutphen_1000"
+
+json_path = "".join([os.getcwd(),"/Data/JSON_files/deventer_2500_land_annotation.json"])
+output_dir = "".join([os.getcwd(),"/Data/Labels/", name, "/"])
 
 
 def extract_rvimage_masks(json_path, output_dir, width = 640, height = 360):
@@ -24,47 +26,48 @@ def extract_rvimage_masks(json_path, output_dir, width = 640, height = 360):
 
 
     for file_path, content in annotations_map.items():
-        if len(content) < 2:
-            continue
-            
-        anno_data = content[0]
-        size_data = content[1]
-        
-        width = size_data.get('w', width)
-        height = size_data.get('h', height)
-
-        # Maak een zwart masker
-        mask = Image.new('RGB', (width, height), (0, 0, 0))
-        draw = ImageDraw.Draw(mask)
-
-        elements = anno_data.get('elts', [])
-        has_labels = False
-
-        for elt in elements:
-            if 'Poly' in elt:
-                poly_data = elt['Poly']
+        if name in file_path:
+            if len(content) < 2:
+                continue
                 
-                # Haal de x,y punten op en zet ze in een lijst van tuples: [(x1, y1), (x2, y2), ...]
-                if 'points' in poly_data:
-                    points_list = []
-                    for p in poly_data['points']:
-                        points_list.append((p['x'], p['y']))
-                    
-                    # Teken de specifieke polygoon (driehoek, ster, etc.) in plaats van een rectangle
-                    if len(points_list) >= 3:
-                        draw.polygon(points_list, fill=(255, 255, 255))
-                        has_labels = True
+            anno_data = content[0]
+            size_data = content[1]
+            
+            width = size_data.get('w', width)
+            height = size_data.get('h', height)
 
-        if has_labels:
-            output_name = os.path.basename(file_path).replace('.jpg', '.jpg')
-            mask.save(os.path.join(output_dir, output_name), format='JPEG')
-            print(f"Opgeslagen: {output_name} (Exacte vorm van {file_path})")
-            image_counter += 1
+            # Maak een zwart masker
+            mask = Image.new('RGB', (width, height), (0, 0, 0))
+            draw = ImageDraw.Draw(mask)
+
+            elements = anno_data.get('elts', [])
+            has_labels = False
+
+            for elt in elements:
+                if 'Poly' in elt:
+                    poly_data = elt['Poly']
+                    
+                    # Haal de x,y punten op en zet ze in een lijst van tuples: [(x1, y1), (x2, y2), ...]
+                    if 'points' in poly_data:
+                        points_list = []
+                        for p in poly_data['points']:
+                            points_list.append((p['x'], p['y']))
+                        
+                        # Teken de specifieke polygoon (driehoek, ster, etc.) in plaats van een rectangle
+                        if len(points_list) >= 3:
+                            draw.polygon(points_list, fill=(255, 255, 255))
+                            has_labels = True
+
+            if has_labels:
+                output_name = os.path.basename(file_path).replace('.jpg', '.jpg')
+                mask.save(os.path.join(output_dir, output_name), format='JPEG')
+                print(f"Opgeslagen: {output_name} (Exacte vorm van {file_path})")
+                image_counter += 1
 
 extract_rvimage_masks(json_path, output_dir)
 
 #voor de plaatjes zonder labels
-for i in range(300):
+for i in range(225):
     if not os.path.exists("".join([output_dir,"image_",str(i),".jpg"])):
                 mask = Image.new('RGB', (input_image_width, input_image_height), (0, 0, 0))
                 mask.save(os.path.join(output_dir, f"image_{i}.jpg"), format='JPEG')
